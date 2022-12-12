5c97f10ac6710d09097fcdfdc8fb89effc2f3c06
Algafiky <103357545+Algafiky@users.noreply.github.com>
2022-12-12 15:26:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+function angkaToRoman($angka)
+{
+	 $angka = intval($angka);
+	 $result = '';
+	 
+	 $array = array('M' => 1000,
+	 'D' => 500,
+	 'C' => 100,
+	 'L' => 50,
+	 'X' => 10,
+	 'V' => 5,
+	 'I' => 1);
+	 
+	 foreach($array as $romawi => $value){
+	  $matches = intval($angka/$value);
+	 
+	  $result .= str_repeat($romawi,$matches);
+	 
+	  $angka = $angka % $value;
+	 } 
+	 return $result;
+}
+	echo angkaToRoman(59);
+?>
